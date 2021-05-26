496c39b0247d859924c41c305db25c1a64530d29
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2021-05-26 20:05:13 +0200

    Update Form4.vb

--- Form4.vb
+++ Form4.vb
@@ -1,26 +1,25 @@
 Public Class Form4
     Dim t_OptionsTemps() As String = {"Aucun", "Désactiver le temps", "Régler le temps à :"}
-    Dim t_OptionsAutre() As String = {"Changez le thème des cartes : ", "Accéder / créer au chemin de sauvegarde", "Activer la pause dans le jeu"}
     Dim t_imagesCartes() As String = {"Test", "Test1", "Test2"}
-
+    Dim time As Integer
 
     Private Sub Quitter_Click(sender As Object, e As EventArgs) Handles Quitter.Click
-
+        Me.Hide()
+        Form1.Show()
     End Sub
 
     Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ThemeCartes.Items.Clear()
         Dim rb As RadioButton
-        Dim cb As CheckBox
         For i As Integer = 0 To Panel1.Controls.Count - 1
             rb = Panel1.Controls(i)
             rb.Text = t_OptionsTemps(i)
             Array.Sort(t_OptionsTemps)
         Next
-        For i As Integer = 0 To Panel2.Controls.Count - 1
-            cb = Panel2.Controls(i)
-            cb.Text = t_OptionsAutre(i)
-            Array.Sort(t_OptionsAutre)
+        For i As Integer = 0 To t_imagesCartes.Count - 1
+            ThemeCartes.Items.Add(t_imagesCartes(i))
         Next
+
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles ChangerTheme.CheckedChanged
@@ -31,15 +30,8 @@ Public Class Form4
         End If
     End Sub
 
-    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles AccederFichier.CheckedChanged
-        If AccederFichier.Checked = True Then
-            AccederFichierSauv.Visible = True
-        Else
-            AccederFichierSauv.Visible = False
-        End If
-    End Sub
 
-    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles PauseJeu.CheckedChanged
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles PauseJeu.CheckedChanged
         If PauseJeu.Checked = True Then
             Form2.Pause.Visible = True
         Else
@@ -50,12 +42,42 @@ Public Class Form4
     Private Sub AccederFichierSauv_Click(sender As Object, e As EventArgs) Handles AccederFichierSauv.Click
         Dim nom_fichier As String = "C:\Users\Wahad\source\repos\ProjetIHM-Mehdi\ProjetIHM-Mehdi\Sauvegarde\sauvegarde.txt"
         If System.IO.File.Exists(nom_fichier) Then
-            Dim stats_Joueur As New System.IO.StreamWriter(nom_fichier)
-            stats_Joueur.Write(Form1.NomJoueur.Text)
-            MessageBox.Show("Texte écrit")
-            stats_Joueur.Close()
+            'Dim stats_Joueur As New System.IO.StreamWriter(nom_fichier, True)
+            'stats_Joueur.Write(Form1.NomJoueur.Text)
+            'MessageBox.Show("Texte écrit")
+            'stats_Joueur.Close()
         Else
             MessageBox.Show("Le fichier n'existe pas")
+        End If
+    End Sub
+
+    Private Sub ReglerTemps_LostFocus(sender As Object, e As EventArgs) Handles ReglerTemps.LostFocus
+        If ReglerTemps.Visible = True Then
+            If ReglerTemps.Text = vbNullString Then
+                Exit Sub
+            End If
+            If IsNumeric(ReglerTemps.Text) Then
+                time = CInt(ReglerTemps.Text)
+                ReglerTemps.Clear()
+            End If
+        End If
+    End Sub
+
+    Public Function getTime()
+        Return time * 60
+    End Function
+
+    Private Sub ReglerTemps_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ReglerTemps.KeyPress
+        If Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If ReglerTemps.Visible = False Then
+            ReglerTemps.Visible = True
+        Else
+            ReglerTemps.Visible = False
         End If
     End Sub
 End Class
